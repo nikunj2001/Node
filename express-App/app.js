9a4d680cd54775fcbe9ec75e48cd5d6630433411
nikunj gupta <59887608+nikunj2001@users.noreply.github.com>
2021-07-17 12:56:02 +0530

    sessions and cookies

--- express-App/app.js
+++ express-App/app.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const session = require('express-session')
 const PORT=3000;
-
+const cookies= require("cookie-parser");
 app.set("views",path.join(__dirname,"views"))
-app.set("view engine","pug");
 
-app.use(express.urlencoded({
-    extended:true
+
+app.use(session({
+        secret:"mysecret",
+        resave:true,
+        saveUninitialized:true
 }))
+app.use(cookies()); 
+let users={
+    name:"Nikunj",
+    age:20
+}
 app.get("/",(req,res)=>{
-    res.render('index',{title:"Form handling"})
+    res.send("Cookies Tutorial");
+});
+app.get("/setuser",(req,res)=>{
+        res.cookie("userData",users);
+        res.send("userdata added to cookie")
+});
+app.get("/getuser",(req,res)=>{
+    res.send(req.cookies)
 })
-app.post("/form_submit",(req,res)=>{
-    const username=req.body.username;
-    const email=req.body.email;
-    res.end(`username is ${username} and email is ${email}`)
+app.get("/logout",(req,res)=>{
+    res.clearCookie("userData");
+    res.send("user logged out");
+})
 
-})
 
 app.listen(PORT,()=>{
     console.log(`App listening on Port ${PORT}`);
 })
+
